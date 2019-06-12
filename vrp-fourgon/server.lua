e291dce5b2dba1b2419b6d93c6baebec6cf4d3ec
Mikou27 <49500798+Mikou27@users.noreply.github.com>
2019-06-12 20:11:12 +0200

    Add files via upload

--- vrp-fourgon/server.lua
+++ vrp-fourgon/server.lua
@@ -9,15 +9,15 @@ vRPclient = Tunnel.getInterface("vRP","vrp-fourgon")
 
 
 RegisterServerEvent('give_ok:give')
-AddEventHandler('give_ok:give', function(ling)             
-local user_id = vRP.getUserId({source})
-    
-	local reward = math.random(22000,50000)
-	if(user_id)then
-     vRP.giveInventoryItem({user_id,"dirty_money",reward,1})			
-	  --vRP.varyExp({user_id,"criminel","braquage",2})
-	 
-     end	
- 
-end)
+AddEventHandler('give_ok:give', function()
 
+local user_id = vRP.getUserId({source})
+
+    local reward = math.random(22000,50000)
+    if(user_id)then
+     vRP.giveInventoryItem({user_id,"dirty_money",reward,1})
+     vRP.varyExp({user_id,"criminel","braquage",2})
+     collectgarbage("collect")
+    end
+
+end)
